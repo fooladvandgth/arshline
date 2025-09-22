01b24096c92ad9eb9d1b1f5811eafee94bf6e300
Nima Azizi <4072801763@Arshian-Co.Com>
2025-09-22 11:19:05 +0330

    استارت افزونه عرشلاین: اولین کامیت

---
+++ arshline.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: استارت افزونه عرشلاین (Arshline Starter)
+Plugin URI: https://example.com/
+Description: این فایل استارت افزونه عرشلاین است و نقطه شروع توسعه می‌باشد.
+Version: 1.0.0
+Author: Your Name
+Author URI: https://example.com/
+License: GPL2
+Text Domain: arshline
+*/
+
+// این فایل استارت افزونه عرشلاین است.
